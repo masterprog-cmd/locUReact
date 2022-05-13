5459cbb9483d1c2f1ea38abc6b3b787e1e5dc427
masterprog-cmd <xaviabadgome01@gmail.com>
2022-05-13 14:27:45 +0200

    .gitignore worked

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native'
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import { Divider } from 'react-native-paper';
